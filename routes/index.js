5a403f7d510510f9dbe19109c004f45cf240e077
Vigneash Sundar <vigneashsundar@live.com>
2014-12-06 14:54:41 +0530

    Working user login checker

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,42 @@ router.get('/login',function(req,res){
 
 })
 
+router.post('/checkuser',function(req,res){
+    
+    var db = req.db;
+    var collection = db.get('userCollection');
+    collection.findOne({"email":req.body.email},function(err,doc){
+        
+           if(doc != null)
+            res.send("Welcome, "+doc.name);
+        else
+            res.redirect('/signup')
+            
+    
+    })
+    
+   
+
+})
+
+router.get('/signup',function(req,res)
+           {
+    
+        res.render('signup',{title:"signup"})
+})
+
+router.post('/adduser',function(req,res){
+
+        var collection = req.db.get("userCollection")
+        var data = {
+        "name":req.body.name,
+            "email":req.body.email,
+            "password":req.body.password
+        }
+        collection.insert(data);
+    console.log('Hey inserted!');
+        res.redirect('/');
+
+})
+
 module.exports = router;
